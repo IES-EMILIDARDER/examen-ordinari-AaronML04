84cb78c458a09f316fac38bed29115e3f2967200
AaronML04 <aaronmatinezlopez04@gmail.com>
2025-06-09 18:07:09 +0200

    Guardado3

--- EXAMEN/src/examen/model/Department.java
+++ EXAMEN/src/examen/model/Department.java
@@ -1,6 +1,6 @@
 package EXAMEN.model;
 
-public class Department {
+public class Department implements Comparable<Deparment>{
     private int departmentId;
     private String name;
     
@@ -17,10 +17,53 @@ public class Department {
         this.departmentId = departmentId;
     }
     
-     // Getters
+    
     public int getId() { 
-        return departmentId; }
+        return departmentId; 
+    }
     
     public String getName() { 
-        return name; } 
+        return name; 
+    } 
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    
+    @Override
+    public int compareTo(Department d) {
+        if (this.departmentId > d.departmentId) return 1;
+        else if (this.departmentId < d.departmentId) return -1;
+        else return 0;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 29 * hash + this.departmentId;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Department other = (Department) obj;
+        return this.departmentId == other.departmentId;
+    }
+
+    @Override
+    public String toString() {
+        return "Department{" + "departmentId=" + departmentId + ", name=" + name + '}';
+    }
+    
+    
 }
--- EXAMEN/src/examen/model/Employee.java
+++ EXAMEN/src/examen/model/Employee.java
@@ -1,6 +1,8 @@
 package EXAMEN.model;
 
-public class Employee {
+import java.util.Objects;
+
+public class Employee implements Comparable<Employee>{
     private String firstName;
     private String lastName;
     private String email;
@@ -14,13 +16,66 @@ public class Employee {
     }
 
     public String getFullName() { 
-        return firstName + lastName; }
+        return firstName + lastName; 
+    }
 
     public Department getDepartment() {
         return department;
     }
     
     public String getEmail() { 
-        return email; }
+        return email; 
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setDepartment(Department department) {
+        this.department = department;
+    }
+    
+    
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Employee other = (Employee) obj;
+        return Objects.equals(this.email, other.email);
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" + "firstName=" + firstName + ", lastName=" + lastName + ", email=" + email + ", department=" + department + '}';
+    }
+
+    @Override
+    public int compareTo(Employee o) {
+       this.equals(o.getEmail());
+        return 0;
+    }
+    
     
 }
